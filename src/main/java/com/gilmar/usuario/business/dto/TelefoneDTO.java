d01b67b429de79efdee0dbb4240a099b3eba1e2f
gilmarborges <146150361+gilmarborges@users.noreply.github.com>
2026-08-15 04:42:56 -0300

    adicionado método que salva dados do usuario

---
+++ src/main/java/com/gilmar/usuario/business/dto/TelefoneDTO.java
@@ -0,0 +1,15 @@
+package com.gilmar.usuario.business.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class TelefoneDTO {
+
+    private String numero;
+    private String ddd;
+}
